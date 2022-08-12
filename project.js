89617d8c728eca668343732fae27a1f882724699
aryan0134 <aryan0134.be20@chitkara.edu.in>
2022-08-12 05:36:42 +0530

    responsive

--- project.js
+++ project.js
@@ -11,3 +11,20 @@ function toggleMenu(){
 }
 
 navIcon.addEventListener("click",toggleMenu);
+
+const xhr=new XMLHttpRequest();
+
+xhr.onload=function(){
+    if(this.status===200){
+    // console.log(this.responseText);
+    const resp=this.responseText;
+    var x=1;
+    for(let i in resp){
+       const objjs=JSON.parse(i);
+       console.log(objjs.title);
+    }
+    }
+};
+
+xhr.open('get','https://jsonplaceholder.typicode.com/posts?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-11');
+xhr.send();
